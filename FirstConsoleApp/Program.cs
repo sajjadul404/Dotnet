ce3e96eb7ab349c12390ff9c357a261ec13dab22
sajjadul404 <sajjadul625@gmail.com>
2025-12-23 21:15:17 +0600

    UniversityName

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -2,22 +2,19 @@
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.Formats.Asn1;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices.Marshalling;
 class Test
 {
     public static void Main(string[] args)
     {
-          string  name = "Sajjadul Islam";
-          double price = 4999.50;
-          bool available = true;
-          int sold =5;
-          string category = "Electronics";
+          string  Universityname = "Bangladesh University of Business and Technology";
+          Console.WriteLine(Universityname);
 
 
-        Console.WriteLine("Name:" + name);
-        Console.WriteLine("Price: $" + price);
-        Console.WriteLine("Category:" + category);
-        Console.WriteLine("Available:" + available);
-        Console.WriteLine("Sold:" + sold);
+            Universityname = "BUBT";
+            Console.WriteLine(Universityname);
 
     }
 }
