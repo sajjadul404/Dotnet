032c2b213ac884a60d052bc285f255ab9dd8182d
sajjadul404 <sajjadul625@gmail.com>
2025-12-23 21:47:39 +0600

    Type custing

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Common;
+using System.Data.SqlTypes;
 using System.Diagnostics;
 using System.Formats.Asn1;
 using System.Reflection.Emit;
+using System.Reflection.Metadata;
 using System.Runtime.InteropServices.Marshalling;
 class Test
 {
     public static void Main(string[] args)
     {
-          string  Universityname = "Bangladesh University of Business and Technology";
-          Console.WriteLine(Universityname);
+       double salary = 4999.50;
+       Console.WriteLine("Salary: $" + salary);
 
-
-            Universityname = "BUBT";
-            Console.WriteLine(Universityname);
-
+       int salary2 = (int)salary;
+         Console.WriteLine("Salary2: $" + salary2);
     }
 }
