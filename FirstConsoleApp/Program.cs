776a9d0a9c17aeeaad60e1682ad35741532a4fb4
sajjadul404 <sajjadul625@gmail.com>
2025-12-24 01:25:21 +0600

    Convert

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -1,21 +1,12 @@
 ﻿using System;
-using System.ComponentModel;
-using System.Data;
-using System.Data.Common;
-using System.Data.SqlTypes;
-using System.Diagnostics;
-using System.Formats.Asn1;
-using System.Reflection.Emit;
-using System.Reflection.Metadata;
-using System.Runtime.InteropServices.Marshalling;
+
 class Test
 {
     public static void Main(string[] args)
     {
-       double salary = 4999.50;
-       Console.WriteLine("Salary: $" + salary);
+        double number = 12345.6789;
+        int number2 = Convert.ToInt32(number);
+        Console.WriteLine($"Converted number: {number2}");
 
-       int salary2 = (int)salary;
-         Console.WriteLine("Salary2: $" + salary2);
     }
 }
