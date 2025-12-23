606b2481183b0dbde63e02da5113a328d8316ef4
sajjadul404 <sajjadul625@gmail.com>
2025-12-23 20:21:49 +0600

    Program

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+class Test
+{
+    public static void Main(string[] args)
+    {
+          string  name = "Sajjadul Islam";
+          double price = 4999.50;
+          bool available = true;
+          int sold =5;
+          string category = "Electronics";
+
+
+        Console.WriteLine("Name:" + name);
+        Console.WriteLine("Price: $" + price);
+        Console.WriteLine("Category:" + category);
+        Console.WriteLine("Available:" + available);
+        Console.WriteLine("Sold:" + sold);
+
+    }
+}
