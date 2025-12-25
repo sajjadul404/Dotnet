896bafaf5f8fe3cef98af3ce6fee50cbec240962
sajjadul404 <sajjadul625@gmail.com>
2025-12-25 13:30:59 +0600

    Student Name

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-
 class Test
 {
     public static void Main(string[] args)
-    {
-        double number = 12345.6789;
-        int number2 = Convert.ToInt32(number);
-        Console.WriteLine($"Converted number: {number2}");
+    {  
+        string studentName = Console.ReadLine();
+        Console.WriteLine("Student Name: " + studentName);
 
     }
 }
