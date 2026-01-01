d2d3cd28f185f6e83e404e2d9bd1fca65cf9cdb4
sajjadul404 <sajjadul625@gmail.com>
2026-01-01 15:27:48 +0600

    Writeline

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -7,11 +7,14 @@ class Test
 
         string? studentName;
         int studentAge;
-        Console.WriteLine("Enter your name:");
+        Console.Write("Enter your name:");
         studentName = Console.ReadLine();
 
-        Console.WriteLine("Enter your age:");
+        Console.Write("Enter your age:");
         studentAge = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Name: " + studentName);
+        Console.WriteLine("Age: " + studentAge + " years");
 
     }
 }
