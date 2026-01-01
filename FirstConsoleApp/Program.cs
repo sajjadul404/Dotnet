0c82029ba1eb5304ff58737da9e9ffed06fcceb6
sajjadul404 <sajjadul625@gmail.com>
2026-01-01 22:44:06 +0600

    Assignmennt

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -7,14 +7,27 @@ class Test
 
         string? studentName;
         int studentAge;
+        double studentGpa;
+        bool studentRegistered;
+        
         Console.Write("Enter your name:");
         studentName = Console.ReadLine();
 
         Console.Write("Enter your age:");
         studentAge = Convert.ToInt32(Console.ReadLine());
 
+        Console.Write("Enter your GPA:");
+        studentGpa = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Have You already Register:");
+        studentRegistered = Convert.ToBoolean(Console.ReadLine());
+
+
+
         Console.WriteLine("Name: " + studentName);
         Console.WriteLine("Age: " + studentAge + " years");
+        Console.WriteLine ("GPA: " + studentGpa);
+        Console.WriteLine ("Registered: " + studentRegistered);
 
     }
 }
