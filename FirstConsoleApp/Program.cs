e718ff315ede0f6cecd7df55fe638bae224e20aa
sajjadul404 <sajjadul625@gmail.com>
2026-01-03 23:49:19 +0600

    Arithmetic operators..

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -6,13 +6,20 @@ class Test
         int number1 = 10;
         int number2 = 3;
 
+        int sum = number1 + number2;
+        Console.WriteLine("sum : " + sum);
 
-        Console.WriteLine(number1 + number2);
-        Console.WriteLine(number1 - number2);
-        Console.WriteLine (number1 * number2);
-        Console.WriteLine(number1 / number2);
-        Console.WriteLine(number1 % number2);
+        int Sub = number1 - number2;
+        Console.WriteLine("Sub : " + Sub);
 
+        int Mul = number1 * number2;
+        Console.WriteLine("Mul : " + Mul);
+
+        int Div = number1 / number2;
+        Console.WriteLine("Div : " + Div);
+
+        int Mod = number1 % number2;
+        Console.WriteLine("Mod : " + Mod);
 
     }
 }
