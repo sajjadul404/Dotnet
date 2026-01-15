616e30b9410753ac5562ff143dcfee720e82a2e1
sajjadul404 <sajjadul625@gmail.com>
2026-01-15 11:18:04 +0600

    digit spelling using if,else

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -4,22 +4,54 @@ class Test
     public static void Main(string[] args)
     {  
     
-        Console.Write("Enter a Nulmber:");
+        Console.Write("Enter a Nulmber(0-9):");
         int number = Convert.ToInt32(Console.ReadLine());
-        if (number > 0)
+
+
+        if (number == 0)
         {
-            Console.WriteLine("Posative Number");
+            Console.WriteLine("Zero");
         }
-        else if (number < 0)
+        else if (number == 1)
         {
-            Console.WriteLine("Negative Number");
+            Console.WriteLine("One");
+        }
+        else if (number == 2)
+        {
+            Console.WriteLine("Two");
+        }
+        else if (number == 3)
+        {
+            Console.WriteLine("Three");
+        }
+        else if (number == 4)
+        {
+            Console.WriteLine("Four");
+        }
+        else if (number == 5)
+        {
+            Console.WriteLine("Five");
+        }
+        else if (number == 6)
+        {
+            Console.WriteLine("Six");
+        }
+        else if (number == 7)
+        {
+            Console.WriteLine("Seven");
+        }
+        else if (number == 8)
+        {
+            Console.WriteLine("Eight");
+        }
+        else if (number == 9)
+        {
+            Console.WriteLine("Nine");
         }
         else
         {
-            Console.WriteLine("Zero");
-
-
-
+            Console.WriteLine("Invalid Input");
         }
+        
     }
 }
