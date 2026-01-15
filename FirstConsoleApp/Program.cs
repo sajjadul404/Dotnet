93609c5dd3d9304ffda9b37b500baec2726ea3aa
sajjadul404 <sajjadul625@gmail.com>
2026-01-15 11:26:13 +0600

    Assignment 7 | even odd program

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -4,53 +4,17 @@ class Test
     public static void Main(string[] args)
     {  
     
-        Console.Write("Enter a Nulmber(0-9):");
+        Console.Write("Enter a Nulmber:");
         int number = Convert.ToInt32(Console.ReadLine());
 
 
-        if (number == 0)
+        if (number % 2 == 0)
         {
-            Console.WriteLine("Zero");
-        }
-        else if (number == 1)
-        {
-            Console.WriteLine("One");
-        }
-        else if (number == 2)
-        {
-            Console.WriteLine("Two");
-        }
-        else if (number == 3)
-        {
-            Console.WriteLine("Three");
-        }
-        else if (number == 4)
-        {
-            Console.WriteLine("Four");
-        }
-        else if (number == 5)
-        {
-            Console.WriteLine("Five");
-        }
-        else if (number == 6)
-        {
-            Console.WriteLine("Six");
-        }
-        else if (number == 7)
-        {
-            Console.WriteLine("Seven");
-        }
-        else if (number == 8)
-        {
-            Console.WriteLine("Eight");
-        }
-        else if (number == 9)
-        {
-            Console.WriteLine("Nine");
+            Console.WriteLine("The number is Even");
         }
         else
         {
-            Console.WriteLine("Invalid Input");
+            Console.WriteLine("The number is Odd");
         }
         
     }
