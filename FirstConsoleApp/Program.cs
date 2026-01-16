08bf7d2cdcd74151a9935cc4eff3c57d15632f5f
sajjadul404 <sajjadul625@gmail.com>
2026-01-16 11:59:17 +0600

    small number

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -11,11 +11,11 @@ class Test
         int number2 = Convert.ToInt32(Console.ReadLine());
 
 
-        if (number1 > number2)
+        if (number1 < number2)
         {
             Console.WriteLine($"{number1} is large number");
         }
-        else if (number2 > number1)
+        else if (number2 < number1)
         {
             Console.WriteLine($"{number2} is large number");
         }
