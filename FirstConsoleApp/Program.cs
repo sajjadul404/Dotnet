493481018ff6d29072f4297d88cba47df396ea6f
sajjadul404 <sajjadul625@gmail.com>
2026-01-17 20:38:57 +0600

    Assignment 9

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -3,29 +3,51 @@ class Test
 {
     public static void Main(string[] args)
     {  
-
-        Console.Write("Enter a Number1:");
-        int number1 = Convert.ToInt32(Console.ReadLine());
-
-        Console.Write("Enter a Number2:");
-        int number2 = Convert.ToInt32(Console.ReadLine());
-
-        Console.Write("Enter a Number3:");
-        int number3 = Convert.ToInt32(Console.ReadLine());
+    
+     Console.Write("Enter your Marks: ");
+     double marks = Convert.ToDouble(Console.ReadLine());
 
 
-        if (number1 > number2 && number1 > number3)
+     if (marks >= 0 && marks <= 100)
         {
-            Console.WriteLine($"{number1} is large number");
-        }
-        else if (number2 > number1 && number2 > number3)
+            if (marks >= 90)
+            {
+                Console.WriteLine ("A+");
+            }
+            else if (marks >= 80)
+            {
+                Console.WriteLine ("A");
+            }
+            else if (marks >= 70)
+            {
+                Console.WriteLine ("B+");
+            }
+            else if (marks >= 60)
+            {
+                Console.WriteLine ("B");
+            }
+            else if (marks >= 50)
+            {
+                Console.WriteLine ("C+");
+            }
+            else if (marks >= 40)
+            {
+                Console.WriteLine ("C");
+            }
+            else if (marks >= 33)
+            {
+                Console.WriteLine ("D");
+            }
+            else
+            {
+                Console.WriteLine ("F");
+            }
+        } 
+        else
         {
-            Console.WriteLine($"{number2} is large number");
+            Console.WriteLine($"{marks} Invalid Marks");
         }
-       else  {
-            Console.WriteLine($"{number3} is large number");
-        }
-
         
+  
     }
 }
