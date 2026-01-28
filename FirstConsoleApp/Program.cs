1fc00a1816a545bd8cedabfb172c46d6fef20f9e
sajjadul404 <sajjadul625@gmail.com>
2026-01-28 23:33:53 +0600

    switch type pattern

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -1,31 +1,25 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Security.Cryptography;
 class Test
 {
     public static void Main(string[] args)
-    {  
-
-       Console.Write("Enter a day of weak : ");
-       string day = Console.ReadLine();
-
-        switch (day.ToLower())
+    {
+        object number = 42;
+        switch (number)
         {
-            case "monday":
-            case "tuesday":
-            case "wednesday":
-            case "thursday":
-            case "friday":
-            Console.WriteLine("It's a weekday.");
-            break;
-            case "saturday":
-            case "sunday":
-            Console.WriteLine("It's a weekend.");
-            break;
+            case int:
+                Console.WriteLine("It's an integer.");
+                break;
+            case string:
+                Console.WriteLine("It's a string.");    
+                break;
+                case double:
+                Console.WriteLine("It's a double.");
+                break;
             default:
-            Console.WriteLine("Invalid day entered.");
-            break; 
+                Console.WriteLine("It's of an unknown type.");
+                break;
         }
-                
-        
     }
 }
