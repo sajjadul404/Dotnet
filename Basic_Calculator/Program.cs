2b30e0178fba1a0ef2e39e971726267cd020890d
sajjadul404 <sajjadul625@gmail.com>
2026-02-02 02:10:26 +0600

    Basic Calculator

--- Basic_Calculator/Program.cs
+++ Basic_Calculator/Program.cs
@@ -3,16 +3,38 @@ class Calculator
 {
     public static void Main(string[] args)
     {
-        int number1, nuimber2;
+        int number1, number2;
         char operation;
 
         Console.Write("Enter an Operation: ");
         operation = Convert.ToChar(Console.ReadLine());
 
-        Console.Write("Enter an operation: ");
-        operation = Convert.ToChar(Console.ReadLine());
+        Console.Write("Enter Number1: ");
+        number1 = Convert.ToInt32(Console.ReadLine());
 
-        
+        Console.Write("Enter Number2: ");
+        number2 = Convert.ToInt32(Console.ReadLine());
+
+        switch (operation)
+        {
+            case '+':
+                Console.WriteLine($"{number1} + {number2} = {number1 + number2}");
+                break;
+            case '-':
+                Console.WriteLine($"{number1} - {number2} = {number1 - number2}");
+                break;
+            case '*':
+                Console.WriteLine($"{number1} * {number2} = {number1 * number2}");
+                break;
+            case '/':   
+                Console.WriteLine($"{number1} / {number2} = {number1 / number2}");
+                break;
+            default:
+                Console.WriteLine("Invalid Operation");
+                break;
+
+                
+        }
 
     }
 }
