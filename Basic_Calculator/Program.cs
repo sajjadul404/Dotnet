b150cbc78eb3ec3cc94a926c59deb242742c1321
sajjadul404 <sajjadul625@gmail.com>
2026-02-04 14:33:17 +0600

    Operator simble add

--- Basic_Calculator/Program.cs
+++ Basic_Calculator/Program.cs
@@ -6,7 +6,7 @@ class Calculator
         Double number1, number2;
         char operation;
 
-        Console.Write("Enter an Operation: ");
+        Console.Write("Enter an Operation(+,-,*,/): ");
         operation = Convert.ToChar(Console.ReadLine());
 
         Console.Write("Enter Number1: ");
