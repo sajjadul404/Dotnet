dd6384638cfea04cd9a8700eb55099684eaa6263
sajjadul404 <sajjadul625@gmail.com>
2026-02-04 14:31:55 +0600

    C:/Program Files/Git/ Operation Update

--- Basic_Calculator/Program.cs
+++ Basic_Calculator/Program.cs
@@ -3,17 +3,17 @@ class Calculator
 {
     public static void Main(string[] args)
     {
-        int number1, number2;
+        Double number1, number2;
         char operation;
 
         Console.Write("Enter an Operation: ");
         operation = Convert.ToChar(Console.ReadLine());
 
         Console.Write("Enter Number1: ");
-        number1 = Convert.ToInt32(Console.ReadLine());
+        number1 = Convert.ToDouble(Console.ReadLine());
 
         Console.Write("Enter Number2: ");
-        number2 = Convert.ToInt32(Console.ReadLine());
+        number2 = Convert.ToDouble(Console.ReadLine());
 
         switch (operation)
         {
@@ -27,12 +27,18 @@ class Calculator
                 Console.WriteLine($"{number1} * {number2} = {number1 * number2}");
                 break;
             case '/':   
-                Console.WriteLine($"{number1} / {number2} = {number1 / number2}");
+                if(number2 != 0)
+                {
+                    Console.WriteLine($"{number1} / {number2} = {number1 / number2}");
+                }
+                else
+                {
+                    Console.WriteLine("Error: Division by zero is not allowed.");
+                }
                 break;
             default:
                 Console.WriteLine("Invalid Operation");
                 break;
-
                 
         }
 
