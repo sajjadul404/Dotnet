11b6d28a1f89bc490bc2ffc5ec6d8c1ea5166ab9
sajjadul404 <sajjadul625@gmail.com>
2026-02-06 01:01:13 +0600

    For Loop

--- Learn_CSharp/Program.cs
+++ Learn_CSharp/Program.cs
@@ -5,23 +5,9 @@ class Test
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Enter a Number 1 to 10:");
-        int Number = Convert.ToInt32(Console.ReadLine());
-
-        switch (Number)
+        for (int i = 0; i < 10; i++)
         {
-            case int num when num < 1 && num > 10:
-                Console.WriteLine("Number is out of range.");
-                break;
-            case int num when num % 2 == 0:
-                Console.WriteLine("Number is Even.");
-                break;
-            case int num when num % 2 != 0:
-                Console.WriteLine("Number is Odd.");
-                break;
-            default:
-                Console.WriteLine("Invalid Input.");
-                break;       
+            Console.WriteLine("Sajjadul Islam");
         }
     }
 }
