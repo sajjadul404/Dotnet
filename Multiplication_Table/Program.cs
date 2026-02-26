c40f851d68df9f8015f6bcd66a8dc54a1e8aff05
sajjadul404 <sajjadul625@gmail.com>
2026-02-27 01:36:05 +0600

    Multiplecation Table

---
+++ Multiplication_Table/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Test
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter a number:");
+        int Startnumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter a number:");
+        int Endnumber = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = Startnumber; i <= Endnumber; i++)
+        {
+            for(int j=1; j <=10; j++)
+            {
+                Console.WriteLine($"{i} x {j} = {i * j}");
+            }
+            Console.WriteLine("-----------------------------");
+        }
+    }
+}
