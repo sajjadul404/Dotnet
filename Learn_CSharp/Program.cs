8b5a4e1d525f867410a50dcb648ec24c14534a5a
sajjadul404 <sajjadul625@gmail.com>
2026-03-12 16:51:48 +0600

    call Greeting Method

--- Learn_CSharp/Program.cs
+++ Learn_CSharp/Program.cs
@@ -3,29 +3,38 @@ using System.Diagnostics;
 using System.Security.Cryptography;
 class Test
 {
+    public static void Greeting()
+    {
+        Console.WriteLine("Welcome to Calculator");
+    }
+    public static void Add()
+    {
+        Console.WriteLine("Adding");
+    }
     public static void Main(string[] args)
     {
-        int number1, number2, result;
+        Greeting();
+        // int number1, number2, result;
        
-        number1 = 10;
-        number2 = 20;
-        result = number1 + number2;
-        Console.WriteLine($"{number1} + {number2} = {result}");
+        // number1 = 10;
+        // number2 = 20;
+        // result = number1 + number2;
+        // Console.WriteLine($"{number1} + {number2} = {result}");
 
-        number1 = 50;
-        number2 = 20;
-        result = number1 + number2;
-        Console.WriteLine($"{number1} + {number2} = {result}");
+        // number1 = 50;
+        // number2 = 20;
+        // result = number1 + number2;
+        // Console.WriteLine($"{number1} + {number2} = {result}");
 
-        number1 = 50;
-        number2 = 20;
-        result = number1 - number2;
-        Console.WriteLine($"{number1} - {number2} = {result}");
+        // number1 = 50;
+        // number2 = 20;
+        // result = number1 - number2;
+        // Console.WriteLine($"{number1} - {number2} = {result}");
 
-        number1 = 80;
-        number2 = 20;
-        result = number1 - number2;
-        Console.WriteLine($"{number1} - {number2} = {result}");
+        // number1 = 80;
+        // number2 = 20;
+        // result = number1 - number2;
+        // Console.WriteLine($"{number1} - {number2} = {result}");
 
 
     }
