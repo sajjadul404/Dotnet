b585d12780de6d12bb4d5df32b74054bfd0f8afc
sajjadul404 <sajjadul625@gmail.com>
2026-03-21 02:58:01 +0600

    Trim

--- Learn_CSharp/Program.cs
+++ Learn_CSharp/Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine($"Enter a number from 1 to 5");
             String input = Console.ReadLine() ?? "";
 
-            input = input.ToLower();
+            input = input.ToLower().Trim();
             if (input == "quit")
             {
                 Console.WriteLine("Thanks for using your app Goodbye!");
