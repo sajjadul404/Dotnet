c6c46a1b3cea7f9ef3c73562be289283d2b62d38
sajjadul404 <sajjadul625@gmail.com>
2026-03-21 02:54:53 +0600

    ToLower

--- Learn_CSharp/Program.cs
+++ Learn_CSharp/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine($"Enter a number from 1 to 5");
             String input = Console.ReadLine() ?? "";
 
+            input = input.ToLower();
             if (input == "quit")
             {
                 Console.WriteLine("Thanks for using your app Goodbye!");
