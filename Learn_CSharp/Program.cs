3521e93f3507863933fb10d2ec2e765a1058fd95
sajjadul404 <sajjadul625@gmail.com>
2026-04-09 01:41:22 +0600

    no update

--- Learn_CSharp/Program.cs
+++ Learn_CSharp/Program.cs
@@ -19,7 +19,6 @@
                 Console.WriteLine("Please enter a valaid input");
                 continue;
             }
-            
             int square = number * number;
             Console.WriteLine($"The square of {number} is {square}");
         }
